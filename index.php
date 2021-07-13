78dd3464eafbc3bde8d1cbaa9ec748c042674310
agrawalsmart7 <agrawalsmart7@gmail.com>
2021-07-13 23:17:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 echo "Hello World!!, this is my first application on Hosted PHP via CICD. This application is vulnerable to many kinds of PHP vulnerabilities"
+  
+  Please access all the labs here:-
+    <a href="xss.php">XSS</a>
+    <a href="xxe.php">xxe</a>
 
 ?>
