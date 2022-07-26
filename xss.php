cae1e9c3993deb1acaf6a7e25b39ccb52c3503af
agrawalsmart7 <agrawalsmart7@gmail.com>
2022-07-26 11:51:36 +0530

    Update xss.php

--- xss.php
+++ xss.php
@@ -2,7 +2,7 @@
   <h2>Hello this code is vulnerable to XSS</h2>
   
   <form action="" method="GET">
-    <input type="text" name="xss" value="<?php echo htmlentities($_GET["testing"]);?>">
+    <input type="text" name="xss" value="<?php echo $_GET["testing"];?>">
   
   </html>
 
